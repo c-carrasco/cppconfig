f7644b7414f7c1a167d85ffab812f91e9dc4afa2
Carlos Carrasco <c.carrascoserrano@king.com>
2023-12-18 10:10:18 +0000

    feat: first version of json tokenizer

---
+++ src/include/cppconfig/json_tokenizer.h
@@ -0,0 +1,276 @@
+// ----------------------------------------------------------------------------
+// MIT License
+//
+// Copyright (c) 2023 Carlos Carrasco
+// ----------------------------------------------------------------------------
+#ifndef __CPP_CONFIG_JSON_TOKENIZER_H__
+#define __CPP_CONFIG_JSON_TOKENIZER_H__
+#include <cstdlib>
+#include <charconv>
+#include <memory>
+#include <optional>
+#include <ostream>
+#include <string>
+#include <string_view>
+
+
+namespace cppconfig::json {
+
+class Buffer {
+  public:
+    Buffer (std::unique_ptr<char []> &&d, size_t len): _data { std::move (d) }, _size { len } {
+      // empty
+    }
+
+    inline char next() {
+      return _data.get()[_idx++];
+    }
+
+
+    inline bool endOfData() const {
+      return _idx >= _size;
+    }
+
+    inline bool match (const char *mem, size_t count) const {
+      if (_idx + count < _size)
+        return std::memcmp (_data.get() + _idx, mem, count) == 0;
+
+      return false;
+    }
+
+    inline bool match (const std::function<bool (char)> &f) const {
+      return (_idx + 1 < _size) && f (_data.get()[_idx + 1]);
+    }
+
+    inline std::pair<int32_t, std::string_view> find (char ch) const {
+      if (const auto it = std::find (_data.get() + _idx, _data.get() + _size, ch); *it == ch) {
+        const auto len { it - (_data.get() + _idx) };
+        return std::make_pair (len, std::string_view { _data.get() + _idx, static_cast<size_t> (len) });
+      }
+
+      return std::make_pair (-1, std::string_view {});
+    }
+
+    inline  size_t count (const std::function<bool (char)> &cb) const {
+      size_t counter { 0 };
+      for (size_t i = _idx; i < _size; ++i) {
+        if (!cb (_data.get()[i]))
+          break;
+
+        ++counter;
+      }
+
+      return counter;
+    }
+
+    inline void forward (size_t len) {
+      _idx += len;
+    }
+
+    inline const char * current() const { return _data.get() + _idx; }
+
+    inline const char * end() const { return _data.get() + _size; }
+
+  private:
+    std::unique_ptr<char []> _data;
+    size_t _size { 0 };
+    size_t _idx { 0 };
+};
+
+enum class JsonTokenId {
+  kObjectBegin,
+  kObjectEnd,
+  kArrayBegin,
+  kArrayEnd,
+  kColon,
+  kComma,
+  kValueString,
+  kValueFloatPoint,
+  kValueInteger,
+  kValueBoolean,
+  kValueNull,
+  kError
+};
+
+class JsonToken {
+  public:
+    JsonToken (JsonTokenId id): _id { id } {
+      // empty
+    }
+
+    JsonToken (bool v): _id { JsonTokenId::kValueBoolean }, _value { v } {
+      // empty
+    }
+
+    JsonToken (double v): _id { JsonTokenId::kValueFloatPoint }, _value { v } {
+      // empty
+    }
+
+    JsonToken (int64_t v): _id { JsonTokenId::kValueInteger }, _value { v } {
+      // empty
+    }
+
+    JsonToken (const std::string_view &v):
+      _id { JsonTokenId::kValueString },
+      _value { std::string { v.begin(), v.end() } }
+    {
+      // empty
+    }
+
+    inline JsonTokenId id() const { return _id; }
+
+    template<typename T>
+    T value() const {
+      return std::get<T> (_value);
+    }
+
+    friend std::ostream & operator<< (std::ostream &os, const JsonToken &obj) {
+      switch (obj._id) {
+        case JsonTokenId::kObjectBegin: os << "ObjectBegin"; break;
+        case JsonTokenId::kObjectEnd: os << "ObjectEnd"; break;
+        case JsonTokenId::kArrayBegin: os << "ArrayBegin"; break;
+        case JsonTokenId::kArrayEnd: os << "ArrayEnd"; break;
+        case JsonTokenId::kColon: os << "Colon"; break;
+        case JsonTokenId::kComma: os << "Comma"; break;
+        case JsonTokenId::kValueString: os << "ValueString[" << std::get<std::string> (obj._value) << "]"; break;
+        case JsonTokenId::kValueFloatPoint: os << "kValueFloatPoint[" << std::get<double> (obj._value) << "]"; break;
+        case JsonTokenId::kValueInteger: os << "kValueInteger[" << std::get<int64_t> (obj._value) << "]"; break;
+        case JsonTokenId::kValueBoolean: os << "ValueBoolean[" << std::get<bool> (obj._value) << "]"; break;
+        case JsonTokenId::kValueNull: os << "ValueNull"; break;
+        case JsonTokenId::kError: os << "Error"; break;
+        default: os << "(UNKNOWN)";
+      }
+
+      return os;
+    }
+
+  private:
+    JsonTokenId _id;
+    std::variant<std::string, bool, int64_t, double> _value;
+};
+
+class JsonTokenizer {
+  public:
+    enum class Error {
+      kNoError,
+      kPrematureEnd
+    };
+
+    JsonTokenizer (Buffer &&buffer): _buffer { std::move (buffer) } {
+      // empty
+    }
+
+    std::optional<JsonToken> next() {
+      while (!_buffer.endOfData()) {
+        const auto c { _buffer.next() };
+
+        switch (c) {
+          case '{': return JsonToken { JsonTokenId::kObjectBegin };
+          case '}': return JsonToken { JsonTokenId::kObjectEnd };
+          case '[': return JsonToken { JsonTokenId::kArrayBegin };
+          case ']': return JsonToken { JsonTokenId::kArrayEnd };
+          case ':': return JsonToken { JsonTokenId::kColon };
+          case ',': return JsonToken { JsonTokenId::kComma };
+          case 'n':
+            if (_buffer.match ("ull", 3)) {
+              _buffer.forward (3);
+              return JsonToken {  JsonTokenId::kValueNull };
+            }
+
+            return _setError (Error::kPrematureEnd);
+          case 't':
+            if (_buffer.match ("rue", 3)) {
+              _buffer.forward (3);
+              return JsonToken { true };
+            }
+
+            return _setError (Error::kPrematureEnd);
+          case 'f':
+            if (_buffer.match ("alse", 4)) {
+              _buffer.forward (4);
+              return JsonToken { false };
+            }
+
+            return _setError (Error::kPrematureEnd);
+          case '"':
+            // TODO: handle '\' + "|\|/|\|b|f|n|r|t|u(4 hex digit)
+            if (const auto [ pos, str ] = _buffer.find ('"'); pos > -1) {
+              _buffer.forward (pos + 1);
+              return JsonToken { str };
+            }
+
+            return _setError (Error::kPrematureEnd);
+          case ' ':
+          case '\t':
+          case '\n':
+          case '\r':
+          case '\0':
+            break;
+          default:
+            if ((c == '-') || std::isdigit (c)) {
+              bool isFp { false };
+
+              const size_t len = _buffer.count ([ &isFp ] (const char c) {
+                switch (c) {
+                  case '.':
+                    isFp = true;
+                    break;
+                  case ' ':
+                  case ',':
+                  case ']':
+                  case '}':
+                  case '\n':
+                    return false;
+                }
+                return true;
+              });
+
+              if (isFp) {
+#if __has_feature(__cpp_lib_constexpr_charconv)
+                double value {};
+                const auto r { std::from_chars (_buffer.current() - 1, _buffer.current() + len, value) };
+                if (r.ptr == _buffer.current() + len) {
+                  _buffer.forward (len);
+                  return JsonToken { value };
+                }
+#else
+                char * end = const_cast<char *> (_buffer.current() + len);
+                const auto value { std::strtod (_buffer.current() - 1, &end) };
+                if ((value != 0) || (end != _buffer.current())) {
+                  _buffer.forward (len);
+                  return JsonToken { value };
+                }
+#endif
+              }
+              else {
+                int64_t value {};
+                const auto r { std::from_chars (_buffer.current() - 1, _buffer.end(), value) };
+                if (r.ptr == _buffer.current() + len) {
+                  _buffer.forward (len);
+                  return JsonToken { value };
+                }
+              }
+            }
+
+            // https://www.tutorialspoint.com/json/json_data_types.htm
+
+            return _setError (Error::kPrematureEnd);
+        }
+      }
+
+      return std::nullopt;
+    }
+
+  private:
+    Buffer _buffer;
+    Error _error { Error::kNoError };
+
+    JsonToken _setError (Error err) {
+      _error = err;
+      return JsonToken { JsonTokenId::kError };
+    }
+};
+
+}
+
+#endif
---
+++ src/test/test_json_token.cxx
@@ -0,0 +1,52 @@
+// ----------------------------------------------------------------------------
+// MIT License
+//
+// Copyright (c) 2023 Carlos Carrasco
+// ----------------------------------------------------------------------------
+#include <gtest/gtest.h>
+
+#include <cppconfig/json_tokenizer.h>
+
+
+// ----------------------------------------------------------------------------
+// test_constructor
+// ----------------------------------------------------------------------------
+TEST (JsonToken, test_constructor) {
+  cppconfig::json::JsonToken jt01 { cppconfig::json::JsonTokenId::kObjectBegin };
+  ASSERT_EQ (jt01.id(), cppconfig::json::JsonTokenId::kObjectBegin);
+  cppconfig::json::JsonToken jt02 { cppconfig::json::JsonTokenId::kObjectEnd };
+  ASSERT_EQ (jt02.id(), cppconfig::json::JsonTokenId::kObjectEnd);
+  cppconfig::json::JsonToken jt03 { cppconfig::json::JsonTokenId::kArrayBegin };
+  ASSERT_EQ (jt03.id(), cppconfig::json::JsonTokenId::kArrayBegin);
+  cppconfig::json::JsonToken jt04 { cppconfig::json::JsonTokenId::kArrayEnd };
+  ASSERT_EQ (jt04.id(), cppconfig::json::JsonTokenId::kArrayEnd);
+  cppconfig::json::JsonToken jt05 { cppconfig::json::JsonTokenId::kColon };
+  ASSERT_EQ (jt05.id(), cppconfig::json::JsonTokenId::kColon);
+  cppconfig::json::JsonToken jt06 { cppconfig::json::JsonTokenId::kComma };
+  ASSERT_EQ (jt06.id(), cppconfig::json::JsonTokenId::kComma);
+  cppconfig::json::JsonToken jt07 { cppconfig::json::JsonTokenId::kValueString };
+  ASSERT_EQ (jt07.id(), cppconfig::json::JsonTokenId::kValueString);
+  cppconfig::json::JsonToken jt08 { cppconfig::json::JsonTokenId::kValueFloatPoint };
+  ASSERT_EQ (jt08.id(), cppconfig::json::JsonTokenId::kValueFloatPoint);
+  cppconfig::json::JsonToken jt09 { cppconfig::json::JsonTokenId::kValueInteger };
+  ASSERT_EQ (jt09.id(), cppconfig::json::JsonTokenId::kValueInteger);
+  cppconfig::json::JsonToken jt10 { cppconfig::json::JsonTokenId::kValueBoolean };
+  ASSERT_EQ (jt10.id(), cppconfig::json::JsonTokenId::kValueBoolean);
+  cppconfig::json::JsonToken jt11 { cppconfig::json::JsonTokenId::kValueNull };
+  ASSERT_EQ (jt11.id(), cppconfig::json::JsonTokenId::kValueNull);
+  cppconfig::json::JsonToken jt12 { cppconfig::json::JsonTokenId::kError };
+  ASSERT_EQ (jt12.id(), cppconfig::json::JsonTokenId::kError);
+
+  cppconfig::json::JsonToken jt13 { std::string_view { "Hello World!" } };
+  ASSERT_EQ (jt13.id(), cppconfig::json::JsonTokenId::kValueString);
+  ASSERT_EQ (jt13.value<std::string>(), "Hello World!");
+  cppconfig::json::JsonToken jt14 { 1.234f };
+  ASSERT_EQ (jt14.id(), cppconfig::json::JsonTokenId::kValueFloatPoint);
+  ASSERT_NEAR (jt14.value<double>(), 1.234, 0.001);
+  cppconfig::json::JsonToken jt15 { -123LL };
+  ASSERT_EQ (jt15.id(), cppconfig::json::JsonTokenId::kValueInteger);
+  ASSERT_EQ (jt15.value<int64_t>(), -123);
+  cppconfig::json::JsonToken jt16 { true };
+  ASSERT_EQ (jt16.id(), cppconfig::json::JsonTokenId::kValueBoolean);
+  ASSERT_EQ (jt16.value<bool>(), true);
+}
